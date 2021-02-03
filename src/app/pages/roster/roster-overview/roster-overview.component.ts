20e11cdebd56098234964bffe5dbb1ed42b08163
Samuel Höra <samuelhoera@gmail.com>
2021-02-03 22:01:52 +0100

    finished overview component

--- src/app/pages/roster/roster-overview/roster-overview.component.ts
+++ src/app/pages/roster/roster-overview/roster-overview.component.ts
@@ -1,7 +1,18 @@
 import { Component } from "@angular/core";
+import { RosterDataService } from "../roster-data.service";
 
 @Component({
   selector: "roster-overview",
+  styleUrls: ["./roster-overview.component.scss"],
   templateUrl: "./roster-overview.component.html",
 })
-export class RosterOverviewComponent {}
+export class RosterOverviewComponent {
+  unfolded = {
+    infanterie: false,
+    armor: false,
+    recon: false,
+    artillery: false,
+  };
+
+  constructor(public service: RosterDataService) {}
+}
