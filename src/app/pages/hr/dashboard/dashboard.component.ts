912cf83780c1a1c0f0deaafb8ac6e57806f33267
Kaffeek0cher <tim.pesch@live.de>
2021-12-27 19:00:40 +0100

    update

--- src/app/pages/hr/dashboard/dashboard.component.ts
+++ src/app/pages/hr/dashboard/dashboard.component.ts
@@ -1,15 +1,4 @@
 import { Component } from "@angular/core";
-import { MembersChangeChartComponent } from "./members-change-chart.component";
-import { MembersPieChartComponent } from "./members-pie-chart.component";
-import { RecruitsComponent } from "./recruits.component";
-import { WarningListComponent } from "./warning-list.component";
-
-export const DASHBOARD_COMPONENTS = [
-  MembersPieChartComponent,
-  MembersChangeChartComponent,
-  RecruitsComponent,
-  WarningListComponent,
-];
 
 @Component({
   selector: "dashboard-component",
